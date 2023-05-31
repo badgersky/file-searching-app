3e0bb870d9660972d78500b5b76b67572eec7255
badgersky <szymonek.ptasznik@gmail.com>
2023-05-31 16:09:18 +0200

    update find file function, create test for finding file with extension added to filename

--- test_utils.py
+++ test_utils.py
@@ -42,3 +42,19 @@ def test_find_file_with_ex(tmp_path):
     result = find_file(tmp_path, filename)
     assert result[0][0] == 'test.txt'
     assert result[0][1] == str(tmp_path / dir_name)
+
+
+def test_find_multiple_files_with_ex(tmp_path):
+    dir_name = 'test-dir'
+    filename = 'test'
+
+    extensions = ['.py', '.txt', '.jpg', '.theme', '.txt']
+    for num, extension in enumerate(extensions, start=1):
+        file = tmp_path / f'{dir_name + str(num)}/{filename + extension}'
+        file.parent.mkdir()
+        file.touch()
+
+    result = find_file(tmp_path, filename + '.txt')
+    filenames = [file[0] for file in result]
+    assert len(result) == 2
+    assert filenames.count('test.txt') == 2
