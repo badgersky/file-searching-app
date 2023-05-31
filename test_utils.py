75ea47d65f64f23effce0df90f2137fdaa944dc1
badgersky <szymonek.ptasznik@gmail.com>
2023-05-31 16:00:43 +0200

    create test for finding multiple files with the same name at different levels of sirectory tree

--- test_utils.py
+++ test_utils.py
@@ -5,10 +5,28 @@ def test_find_file_no_ex(tmp_path):
     dir_name = 'test-dir'
     filename = 'test'
 
-    file = tmp_path / f"{dir_name}/test.txt"
+    file = tmp_path / f'{dir_name}/test.txt'
     file.parent.mkdir()
     file.touch()
 
     result = find_file(tmp_path, filename)
     assert result[0][0] == 'test.txt'
     assert result[0][1] == str(tmp_path / dir_name)
+
+
+def test_find_multiple_files_no_ex(tmp_path):
+    dir_name = 'test-dir'
+    filename = 'test'
+
+    extensions = ['.py', '.txt', '.jpg', '.theme', '.txt']
+    for num, extension in enumerate(extensions, start=1):
+        file = tmp_path / f'{dir_name + str(num)}/{filename + extension}'
+        file.parent.mkdir()
+        file.touch()
+
+    result = find_file(tmp_path, filename)
+    filenames = [file[0] for file in result]
+    assert len(result) == 5
+    assert filenames.count('test.txt') == 2
+    assert filenames.count('test.py') == 1
+    
