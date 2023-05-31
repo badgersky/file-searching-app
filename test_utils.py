7e900b3a2fdc5cafa71bbe13e89e02854ad18719
badgersky <szymonek.ptasznik@gmail.com>
2023-05-31 16:08:22 +0200

    update find file function, create test for finding file with extension added to filename

--- test_utils.py
+++ test_utils.py
@@ -29,4 +29,16 @@ def test_find_multiple_files_no_ex(tmp_path):
     assert len(result) == 5
     assert filenames.count('test.txt') == 2
     assert filenames.count('test.py') == 1
-    
+
+
+def test_find_file_with_ex(tmp_path):
+    dir_name = 'test-dir'
+    filename = 'test.txt'
+
+    file = tmp_path / f'{dir_name}/test.txt'
+    file.parent.mkdir()
+    file.touch()
+
+    result = find_file(tmp_path, filename)
+    assert result[0][0] == 'test.txt'
+    assert result[0][1] == str(tmp_path / dir_name)
--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import os
 
 
-def find_file(path, filename, extension=None):
-    if not extension:
+def find_file(path, filename):
+    if '.' not in filename:
         result = []
         for paths, dirs, files in os.walk(path):
             file_no_ex = [os.path.splitext(file)[0] for file in files]
@@ -10,7 +10,6 @@ def find_file(path, filename, extension=None):
             if filename in file_no_ex:
                 result.append((files[file_no_ex.index(filename)], paths))
     else:
-        filename = filename + '.' + extension
         result = [(files[files.index(filename)], paths) for paths, dirs, files in os.walk(path) if filename in files]
 
     if not result:
@@ -22,8 +21,8 @@ if __name__ == '__main__':
     print('-' * 40)
     print(find_file(os.environ.get('HOME'), 'empire_invaders'))
     print('-' * 40)
-    print(find_file(os.environ.get('HOME'), 'empire_invaders', 'py'))
+    print(find_file(os.environ.get('HOME'), 'empire_invaders.py'))
     print('-' * 40)
     print(find_file(os.environ.get('HOME'), 'test'))
     print('-' * 40)
-    print(find_file(os.environ.get('HOME'), 'test', 'txt'))
+    print(find_file(os.environ.get('HOME'), 'test.txt'))
